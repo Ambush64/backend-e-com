cdcdffb82237324d68e822e1a6ff6abe0fdbcb66
Ambush64 <bilal57ei@gmail.com>
2023-10-29 13:43:28 +0530

    reconnect

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,39 +1,49 @@
 const mysql = require('mysql');
 
 function connectDB() {
+  const dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+  };
+
   let connection;
 
   function createConnection() {
-    connection = mysql.createConnection({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-    });
+    connection = mysql.createConnection(dbConfig);
 
     connection.connect((err) => {
       if (err) {
         console.error('MySQL Database connection FAIL');
-        console.error(err); // Log the error for debugging
-
-        // Attempt to reconnect after a delay (e.g., 5 seconds)
-        setTimeout(createConnection, 5000);
+        console.error(err);
+        reconnect();
       } else {
         console.log('MySQL Database connection SUCCESS');
+        handleDisconnect(connection);
       }
     });
+  }
 
-    connection.on('error', (err) => {
+  function reconnect() {
+    setTimeout(() => {
+      console.log('Reconnecting to MySQL...');
+      createConnection();
+    }, 5000);
+  }
+
+  createConnection();
+
+  function handleDisconnect(db) {
+    db.on('error', (err) => {
       if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-        // Connection was lost, attempt to reconnect
-        createConnection();
+        console.error('MySQL connection lost, attempting to reconnect...');
+        reconnect();
       } else {
         throw err;
       }
     });
   }
-
-  createConnection();
 
   return connection;
 }
